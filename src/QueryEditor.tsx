70e0653c1499e01acda37d5f7d4ea15ca48828ed
Mr-Xzz <1191054117@qq.com>
2023-06-21 16:27:59 +0800

    feat: add xcol ycol introduce tip, upgrade input to grafana ui.

--- src/QueryEditor.tsx
+++ src/QueryEditor.tsx
@@ -1,12 +1,15 @@
 import { defaults } from 'lodash';
 
 import React, { ChangeEvent, PureComponent } from 'react';
-import { LegacyForms, InlineFormLabel } from '@grafana/ui';
+import { InlineFormLabel, Icon, Input, Tooltip, InlineField, Card, SeriesTable } from '@grafana/ui';
 import { QueryEditorProps } from '@grafana/data';
+// import { css } from '@emotion/css';
+
 import { SLSDataSource } from './datasource';
 import { defaultQuery, SLSDataSourceOptions, SLSQuery } from './types';
+import { xColInfoSeries, yColInfoSeries } from 'const';
 
-const { FormField } = LegacyForms;
+// const { FormField } = LegacyForms;
 
 type Props = QueryEditorProps<SLSDataSource, SLSQuery, SLSDataSourceOptions>;
 
@@ -36,7 +39,7 @@ export class SLSQueryEditor extends PureComponent<Props> {
 
     return (
       <>
-        <div className="gf-form-inline">
+        <div className="gf-form gf-form--grow flex-shrink-1 min-width-15">
           <InlineFormLabel width={6} className="query-keyword">
             Query
           </InlineFormLabel>
@@ -45,11 +48,61 @@ export class SLSQueryEditor extends PureComponent<Props> {
             value={query}
             onChange={this.onQueryTextChange}
             onBlur={this.onQueryTextChange}
+            style={{ fontFamily: 'Menlo, monospace' }}
           ></input>
         </div>
-        <div className="gf-form-inline">
-          <FormField labelWidth={6} inputWidth={30} value={ycol} onChange={this.onYChange} label="ycol" />
-          <FormField labelWidth={6} inputWidth={20} value={xcol} onChange={this.onXChange} label="xcol(time)" />
+        <div className="gf-form-inline" style={{ lineHeight: '32px', verticalAlign: 'center' }}>
+          <InlineField label={'ycol'} labelWidth={12}>
+            <Input
+              width={60}
+              prefix={<Icon name="text-fields" />}
+              value={ycol}
+              onChange={this.onYChange}
+              label="ycol"
+              suffix={
+                <Tooltip
+                  content={
+                    <Card>
+                      <Card.Heading>ycol 简介 Introduction</Card.Heading>
+                      <Card.Description>
+                        <SeriesTable series={yColInfoSeries} />
+                      </Card.Description>
+                    </Card>
+                  }
+                  interactive
+                  theme="info-alt"
+                >
+                  <Icon name="question-circle" />
+                </Tooltip>
+              }
+            />
+          </InlineField>
+          <InlineField label={'xcol(time)'} labelWidth={12}>
+            <Input
+              style={{ fontFamily: 'Menlo, monospace' }}
+              width={40}
+              prefix={<Icon name="x" />}
+              value={xcol}
+              onChange={this.onXChange}
+              label="xcol(time)"
+              suffix={
+                <Tooltip
+                  content={
+                    <Card>
+                      <Card.Heading>xcol 简介 Introduction</Card.Heading>
+                      <Card.Description>
+                        <SeriesTable series={xColInfoSeries} />
+                      </Card.Description>
+                    </Card>
+                  }
+                  interactive
+                  theme="info-alt"
+                >
+                  <Icon name="question-circle" />
+                </Tooltip>
+              }
+            />
+          </InlineField>
         </div>
       </>
     );
---
+++ src/const.ts
@@ -0,0 +1,25 @@
+export const xColInfoSeries = [
+  {
+    color: '#299c46',
+    label: '空 empty',
+    value: '表格全列或日志 full table or log',
+  },
+  {
+    color: '#eb7b18',
+    label: 'bar',
+    value: '柱状图格式 bar graph',
+  },
+];
+
+export const yColInfoSeries = [
+    {
+      color: '#299c46',
+      label: '空 empty',
+      value: '表格全列或日志 full table or log',
+    },
+    {
+      color: '#eb7b18',
+      label: 'bar',
+      value: '柱状图格式 bar graph',
+    },
+  ];
